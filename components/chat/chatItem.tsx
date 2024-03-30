40dc246af38cc5ae799dba3dcf6c23e634a6f001
sohye-lee <sohyelee@gmail.com>
2024-03-30 07:16:49 -0400

    fix

--- components/chat/chatItem.tsx
+++ components/chat/chatItem.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { IconRobotFace } from '@tabler/icons-react';
+import { IconRobotFace, IconUser } from '@tabler/icons-react';
 import { ChatCompletionContentPart } from 'openai/resources/chat/completions.mjs';
 import { useMutation } from '@tanstack/react-query';
 import { getUserInfo } from '@/utils/action';
@@ -14,34 +14,35 @@ interface ChatItemProps {
 }
 
 export default function ChatItem({ role, content }: ChatItemProps) {
-  const [image, setImage] = useState<string>('');
-  const { mutate, isPending } = useMutation({
-    mutationFn: () => getUserInfo(),
-    onSuccess: (data) => {
-      setImage(data?.user?.image ?? '');
-    },
-  });
+  // const [image, setImage] = useState<string>('');
+  // const { mutate, isPending } = useMutation({
+  //   mutationFn: () => getUserInfo(),
+  //   onSuccess: (data) => {
+  //     setImage(data?.user?.image ?? '');
+  //   },
+  // });
 
-  useEffect(() => {
-    image == '' && mutate();
-  }, [image, mutate]);
+  // useEffect(() => {
+  //   image == '' && mutate();
+  // }, [image, mutate]);
   return (
     <div className="group relative flex items-start w-full py-4 px-3 border last:border-none border-b-slate-100 dark:border-t-slate-700 ">
       {role == 'user' ? (
         <div
-          className={`w-8 h-8 relative border border-gray-300 bg-gray-50 border-md flex items-center justify-center rounded-md bg-brand/25`}
+          className={`w-8 h-8 relative border border-gray-300 flex items-center justify-center rounded-md bg-gradient-to-r from-brand to-blue`}
         >
-          {image && image != '' && (
+          <IconUser width={20} />
+          {/* {image && image != '' && (
             <Image
               src={image}
               alt=""
               fill
               className="min-w-full min-h-full w-auto h-auto group-last:animate-ping"
             />
-          )}
+          )} */}
         </div>
       ) : (
-        <div className="w-8 h-8 border border-gray-300 bg-gray-50 border-md flex items-center justify-center rounded-md  ">
+        <div className="w-8 h-8 border border-gray-300 bg-gray-50  flex items-center justify-center rounded-md  ">
           <Image
             src={Logo}
             alt="bot"
