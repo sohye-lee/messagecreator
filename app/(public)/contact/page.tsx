37015c78e3cfa174dfa75b5d95a793be92214fcb
Sohye Lee <sohyelee@gmail.com>
2024-04-11 16:03:35 -0400

    contact page

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -15,6 +15,8 @@ import { useRouter } from "next/navigation";
 import React, { FormEvent, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { toast } from "sonner";
+import Letter from "@images/illust_letter.png";
+import Image from "next/image";
 
 export default function ContactPage() {
   const router = useRouter();
@@ -43,15 +45,21 @@ export default function ContactPage() {
   };
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="w-full pt-20 pb-6 bg-gradient-to-r from-brand to-gray-700">
+      {/* <div className="w-full pt-20 pb-6 bg-gradient-to-r from-brand to-gray-700">
         <div className="mx-auto w-full sm:max-w-xl flex flex-col gap-3 pt-2">
-          <h1 className="text-3xl font-medium text-center text-white">
-            Contact Us
-          </h1>
         </div>
-      </div>
-      <div className=" w-full overflow-auto flex flex-col items-center px-4">
+      </div> */}
+      <div className=" w-full overflow-auto flex flex-col items-center px-4 pt-16">
         <div className="mx-auto w-full sm:max-w-xl flex flex-col gap-3 py-14 ">
+          <div className="flex items-center gap-2">
+            <Image src={Letter} alt="letter" width={80} />
+            <h1 className="text-4xl font-medium mb-3 ">Tell me anything!</h1>
+          </div>
+          <h5 className="text-md mb-8">
+            Fill out the form to reach out to us. Questions, feedback, or just a
+            quick hello — we&apos;re here for it all and can&apos;t wait to hear
+            from you.
+          </h5>
           <form
             className="flex flex-col gap-3"
             onSubmit={handleSubmit(onSubmit)}
